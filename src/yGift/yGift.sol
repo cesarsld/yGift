d38d5ccb88e3ec9ad50dca26dd5ad77c6452be49
cesarsld <jaimes.cesar88@gmail.com>
2020-10-15 17:40:36 +0200

    Add support for NFT and 1155

--- src/yGift/yGift.sol
+++ src/yGift/yGift.sol
@@ -5,6 +5,44 @@ import "../erc20/IERC20.sol";
 import "./Controller.sol";
 import "../utils/SafeMath.sol";
 
+interface IERC1155 {
+
+
+    /**
+        @notice Transfers `_value` amount of an `_id` from the `_from` address to the `_to` address specified (with safety call).
+        @dev Caller must be approved to manage the tokens being transferred out of the `_from` account (see "Approval" section of the standard).
+        MUST revert if `_to` is the zero address.
+        MUST revert if balance of holder for token `_id` is lower than the `_value` sent.
+        MUST revert on any other error.
+        MUST emit the `TransferSingle` event to reflect the balance change (see "Safe Transfer Rules" section of the standard).
+        After the above conditions are met, this function MUST check if `_to` is a smart contract (e.g. code size > 0). If so, it MUST call `onERC1155Received` on `_to` and act appropriately (see "Safe Transfer Rules" section of the standard).        
+        @param _from    Source address
+        @param _to      Target address
+        @param _id      ID of the token type
+        @param _value   Transfer amount
+        @param _data    Additional data with no specified format, MUST be sent unaltered in call to `onERC1155Received` on `_to`
+    */
+    function safeTransferFrom(address _from, address _to, uint256 _id, uint256 _value, bytes calldata _data) external;
+
+    /**
+        @notice Transfers `_values` amount(s) of `_ids` from the `_from` address to the `_to` address specified (with safety call).
+        @dev Caller must be approved to manage the tokens being transferred out of the `_from` account (see "Approval" section of the standard).
+        MUST revert if `_to` is the zero address.
+        MUST revert if length of `_ids` is not the same as length of `_values`.
+        MUST revert if any of the balance(s) of the holder(s) for token(s) in `_ids` is lower than the respective amount(s) in `_values` sent to the recipient.
+        MUST revert on any other error.        
+        MUST emit `TransferSingle` or `TransferBatch` event(s) such that all the balance changes are reflected (see "Safe Transfer Rules" section of the standard).
+        Balance changes and events MUST follow the ordering of the arrays (_ids[0]/_values[0] before _ids[1]/_values[1], etc).
+        After the above conditions for the transfer(s) in the batch are met, this function MUST check if `_to` is a smart contract (e.g. code size > 0). If so, it MUST call the relevant `ERC1155TokenReceiver` hook(s) on `_to` and act appropriately (see "Safe Transfer Rules" section of the standard).                      
+        @param _from    Source address
+        @param _to      Target address
+        @param _ids     IDs of each token type (order and length must match _values array)
+        @param _values  Transfer amounts per token type (order and length must match _ids array)
+        @param _data    Additional data with no specified format, MUST be sent unaltered in call to the `ERC1155TokenReceiver` hook(s) on `_to`
+    */
+    function safeBatchTransferFrom(address _from, address _to, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external;
+}
+
 library SafeERC20 {
 	using SafeMath for uint256;
 	using Address for address;
@@ -49,7 +87,10 @@ contract yGift is ERC721("yearn Gift NFT", "yGIFT"), Controller {
 		address	recipient;
 		address	token;
 		uint256	amount;
-		string	imageURL;
+		bool	artType;
+		address	artContract;
+		uint256	tokenId
+		uint256	value;
 		bool	redeemed;
 		uint256	createdAt;
 		uint256	lockedDuration;
@@ -108,7 +149,10 @@ contract yGift is ERC721("yearn Gift NFT", "yGIFT"), Controller {
 		gift.recipient,
 		gift.token,
 		gift.amount,
-		gift.imageURL,
+		gift.artType,
+		gift.artContract,
+		gift.tokenId,
+		goft.value,
 		gift.redeemed,
 		gift.createdAt,
 		gift.lockedDuration
@@ -131,9 +175,12 @@ contract yGift is ERC721("yearn Gift NFT", "yGIFT"), Controller {
 	 */
 	function mint(
 		address _to,
-		address _token,
+		address _ercToken,
 		uint256 _amount,
-		string calldata _url,
+		bool	_artType;
+		address	_artContract,
+		uint	_tokenId
+		uint	_value,
 		string calldata _name,
 		string calldata _msg,
 		uint256 _lockedDuration)
@@ -143,11 +190,27 @@ contract yGift is ERC721("yearn Gift NFT", "yGIFT"), Controller {
 		require(_lockedDuration <= MAX_LOCK_PERIOD, "yGift: Locked period is too large");
 
 		uint256 _id = gifts.length;
-		Gift memory gift = Gift(_name, msg.sender, _to, _token, _amount, _url, false, block.timestamp, _lockedDuration);
+		Gift memory gift = Gift(
+			_name,
+			msg.sender,
+			_to,
+			_token,
+			_amount,
+			_artType,
+			_artContract,
+			_tokenId,
+			_value,
+			false,
+			block.timestamp,
+			_lockedDuration);
 		gifts.push(gift);
 		tokensHeld[_token] = tokensHeld[_token].add(_amount);
 		_safeMint(address(this), _id);
 		IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
+		if (_artType)
+			IERC721(_artContract).safeTransferFrom(msg.sender, address(this), _tokenId);
+		else
+			IERC1155(_artContract).safeTransferFrom(msg.sender, address(this), _tokenId, _value, "");
 		emit Tip(msg.sender, _id, _token, _amount, _msg);
 	}
 
@@ -176,13 +239,32 @@ contract yGift is ERC721("yearn Gift NFT", "yGIFT"), Controller {
 	 *
 	 * requirement: caller must own the gift recipient && function must be called after the locked duration
 	 */
-	function redeem(uint256 _tokenId) public {
+	function redeemTokens(uint256 _tokenId) public {
 		require(_tokenId < gifts.length, "yGift: Token ID does not exist.");
 		Gift storage gift = gifts[_tokenId];
 		require(msg.sender == gift.recipient, "yGift: You are not the recipient.");
 		require(gift.createdAt.add(gift.lockedDuration) >= block.timestamp, "yGift: Gift is still locked.");
 		gift.redeemed = true;
 		_safeTransfer(address(this), msg.sender, _tokenId, "");
+	}
+
+	function collectArt(uint256 _tokenId) public {
+		require(_tokenId < gifts.length, "yGift: Token ID does not exist.");
+		require(ownerOf(_tokenId) == msg.sender, "yGift: You are not the NFT owner.");
+		Gift storage gift = gifts[_tokenId];
+		require(gift.redeemed, "yGift: NFT tokens cannot be collected.");
+		address _artContract = gift.artContract;
+		uint _tokenId = gift.tokenId;
+		uint _value = gift.value;
+		gift.artContract = address(0);
+		gift.tokenId = 0;
+		gift.valuee = 0;
+		if (gift.artType) {
+			IERC721(_artContract).safeTransferFrom(address(this), msg.sender, _tokenId);
+		}
+		else {
+			IERC1155(_artContract).safeTransferFrom(address(this), msg.sender, _tokenId, _value);
+		}
 	}
 
 
@@ -218,7 +300,14 @@ contract yGift is ERC721("yearn Gift NFT", "yGIFT"), Controller {
 	}
 
 	function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes calldata _data) external view returns (bytes4) {
-		require(msg.sender == address(this), "yGift: Cannot receive other NFTs");
 		return yGift.onERC721Received.selector;
+	}
+
+	function onERC1155Received(address _operator, address _from, uint256 _id, uint256 _value, bytes calldata _data) external returns(bytes4) {
+		return yGift.onERC1155Received.selector;
+	}
+
+	function onERC1155BatchReceived(address _operator, address _from, uint256[] calldata _ids, uint256[] calldata _values, bytes calldata _data) external returns(bytes4) {
+		return yGift.onERC1155BatchReceived.selector;
 	}
 }
