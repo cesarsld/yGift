d31be5cbb3f656a532bcead61d1e86c22759219a
cesarsld <jaimes.cesar88@gmail.com>
2020-11-19 14:13:54 +0100

    Add stateful test

--- tests/test_state_machine.py
+++ tests/test_state_machine.py
@@ -7,7 +7,7 @@ from brownie.test import strategy
 def test_stateful(ygift, token, chain, giftee, receiver, state_machine):
     class StateMachine:
 
-        # address = strategy('address')
+        address = strategy('address')
         amount = strategy("uint256", max_value="100 ether")
         start = strategy("uint256", max_value="1000000")
         duration = strategy("uint256", max_value="1500000")
@@ -21,36 +21,36 @@ def test_stateful(ygift, token, chain, giftee, receiver, state_machine):
 
         def setup(self):
             self.token.approve(ygift, 2 ** 256 - 1)
-
-        def rule_mint(self, amount, start, duration):
             time = self.chain[-1].timestamp
-            f_start = start + time
-            f_duration = duration
+            f_start = 500000 + time
+            f_duration = 750000
             self.ygift.mint(
                 self.giftee,
                 self.token,
-                amount,
+                Wei("200 ether"),
                 "name",
                 "msg",
                 "url",
                 f_start,
                 f_duration,
             )
-            gift = ygift.gifts(ygift.totalSupply() - 1).dict()
-            assert gift == {
-                "token": self.token,
-                "name": "name",
-                "message": "msg",
-                "url": "url",
-                "amount": amount,
-                "tipped": 0,
-                "start": f_start,
-                "duration": f_duration,
-            }
 
-        # def rule_collect_half(self, amount):
-        # 	ygift.collect(ygift.totalSupply() - 1, amount / 2, {'from':self.giftee})
-        # 	gift = ygift.gifts(ygift.totalSupply() - 1).dict()
-        # 	assert self.token.balanceOf(self.giftee) + gift['amount']  ==  amount
 
-    # state_machine(StateMachine, ygift, token, chain, giftee, receiver)
+        def rule_try_redeem(self, amount, address, duration):
+            self.chain.sleep(duration)
+            if self.ygift.totalSupply() > 0:
+                gift = self.ygift.gifts(0).dict()
+                expected = self.ygift.collectible(0)
+                if self.ygift.ownerOf(0) != address:
+                    with brownie.reverts("yGift: You are not the NFT owner"):
+                        self.ygift.collect(0, amount, {'from': address})
+                else:
+                    if gift["start"] <= self.chain[-1].timestamp + duration:
+                        before = self.token.balanceOf(address)
+                        self.ygift.collect(0, expected, {'from': address})
+                        assert self.token.balanceOf(address) == expected + before
+                    else:
+                        with brownie.reverts("yGift: Rewards still vesting"):
+                            self.ygift.collect(0, amount, {'from': address})
+
+    state_machine(StateMachine, ygift, token, chain, giftee, receiver)
